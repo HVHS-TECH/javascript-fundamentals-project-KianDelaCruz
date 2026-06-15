90f78d83a74214f7f8dc0074767fac2fe084b1b8
26590kd <26590kd@hvhs.school.nz>
2026-06-15 13:31:30 +1200

    BASICS

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello world!")
+var name = "Kian"
+var year = 2026
+var age = 17
+var dollars = 12
+var birthdate = "June 21st 2008"
+
+console.log("Hi " + name)
+console.log("As of " + year + " you are " + age + " years old")
+console.log("You were born in " + birthdate)
+console.log("In ten years you will be" , age + 10 , "years old")
+console.log("You have " + dollars + " dollars")
+console.log("You spend half of your money, now you have", dollars / 2)
+console.log("Then you get $3 now you have", dollars / 2 + 3)
