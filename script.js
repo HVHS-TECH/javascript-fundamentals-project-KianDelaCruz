e4323fd1a4207571081fd48f4fe934dc32a46fb4
26590kd <26590kd@hvhs.school.nz>
2026-06-16 15:13:52 +1200

    func

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-var name = "Kian"
-var year = 2026
-var age = 17
-var dollars = 12
-var birthdate = "June 21st 2008"
 
-console.log("Hi " + name)
-console.log("As of " + year + " you are " + age + " years old")
-console.log("You were born in " + birthdate)
-console.log("In ten years you will be" , age + 10 , "years old")
-console.log("You have " + dollars + " dollars")
-console.log("You spend half of your money, now you have", dollars / 2)
-console.log("Then you get $3 now you have", dollars / 2 + 3)
+
+function getFormInput(){
+    const NAME = document.getElementById("name_field");
+    let username = NAME.value;
+    OUTPUT.innerHTML = "<p> Your user name is " + username +"</p>";
+}
+
+const OUTPUT = document.getElementById("script.js");
+OUTPUT.innerHTML = "<h2>Hi " + username + "<h2>";
